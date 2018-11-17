fe5358fa74343eb1eac2e0e54e975bc6c823b50d
Thoa T <thoat@users.noreply.github.com>
2018-11-17 10:26:37 -0500

    Set up ESLint

--- server.js
+++ server.js
@@ -3,20 +3,21 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+
 const port = process.env.PORT || 3000;
 
 // PART 1: Open DB connection
 
 const prod = process.env.NODE_ENV === 'production';
 if (prod) {
-  app.use(express.static(__dirname + '/build'));
+  app.use(express.static(`${__dirname}/build`));
   app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/build/index.html');
+    res.sendFile(`${__dirname}/build/index.html`);
   });
 } else {
-  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(`${__dirname}/public`));
   app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(`${__dirname}/public/index.html`);
   });
 }
 
