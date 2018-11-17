7cb1e2c3add59dce6839946ddba1a928083ae6c6
Thoa T <thoat@users.noreply.github.com>
2018-11-16 23:14:17 -0500

    Commit work end of day 2018/11/16

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+// PART 1: Open DB connection
+
+const prod = process.env.NODE_ENV === 'production';
+if (prod) {
+  app.use(express.static(__dirname + '/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/build/index.html');
+  });
+} else {
+  app.use(express.static(__dirname + '/public'));
+  app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+  });
+}
+
+io.on('connection', (socket) => {
+  // here you can start emitting events to the client
+  socket.emit('event name', { some: 'data' });
+});
+
+// PART 2: WebSocket & events
+
+// const port = 8000;
+// io.listen(port);
+// console.log('Listening on port ', port);
